911c60d35d703c97e70de03c1064061968cd2b2d
Andrea Cesare Trapani <161830645+AndreaTrapani97@users.noreply.github.com>
2025-07-17 17:24:01 +0200

    aggiunto js

---
+++ navigare_DOM.js
@@ -0,0 +1,9 @@
+let ul = document.querySelector("ul");
+let body = ul.parentNode;
+console.log(body);
+let li2 = ul.children[1];
+console.log(li2);
+let li3 = li2.nextElementSibling;
+console.log(li3);
+let li1 = li2.previousElementSibling;
+console.log(li1);
