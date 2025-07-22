6e2b77025708ea6bcbedc54eec69f94302c6b409
Andrea Cesare Trapani <161830645+AndreaTrapani97@users.noreply.github.com>
2025-07-22 17:05:12 +0200

    correzione manipolazione DOM1

--- manipolazione_DOM1/index.js
+++ manipolazione_DOM1/index.js
@@ -7,7 +7,7 @@ $btn.addEventListener("click", aggiungiLista);
 function aggiungiLista() {
   const li = document.createElement("li");
   let value = $input.value;
-  li.innerHTML += `<li>${value}</li><input type="checkbox">`;
+  li.innerHTML += `${value}<input type="checkbox">`;
   $ul.appendChild(li);
   $input.value = "";
 }
